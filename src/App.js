89fb7c1a0103befccf1c28de3a8f16244e0ed462
Shubham Bansal <shubh.6102001@gmail.com>
2024-01-24 14:06:36 +0530

    Home,Product Card,Navbar

--- src/App.js
+++ src/App.js
@@ -3,27 +3,46 @@ import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
+import Navbar from './Navbar';
 
 const App=()=> {
+
   const [data, setdata] = useState('')
+
 useEffect(() => {
 const fetchData= async () => {
   const data=await axios.get('http://localhost:3030/data');
   setdata(data.data);
 };
 fetchData();
-
 },[])
-if(!data) {console.log("Loading");return <div>Loading ....</div>;}
-console.log(data[0]);
 
+
+if(!data) {console.log("Loading");return <div>Loading ....</div>;}
   return (
-    <div className='flex flex-col'>
+    <>
+    <Navbar/>
+    <div className=''>
+<form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative m-8 mt-24 md:m-24">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="p-4 md:w-10/12 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required/>
+        <button type="submit" class="absolute text-white ml-2 lg:ml-4  bg-blue-700 md:w-32 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-4 ">Search</button>
+    </div>
+</form>
+</div>
+    <div className='flex flex-wrap gap-6 justify-evenly'>
       {data.map((e,i)=>{
-        return <ProductCard data={e} key={i}/>
+        return <ProductCard data={e} key={i} src="https://t4.ftcdn.net/jpg/02/81/42/77/360_F_281427785_gfahY8bX4VYCGo6jlfO8St38wS9cJQop.jpg"/>
       })}
       
    </div>
+   </>
   );
 }
 
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProductCard = ({data}) => {
+const ProductCard = ({data,src}) => {
   return (
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="size-4/5 md:size-3/12 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="p-8 rounded-t-lg" src={data.image} alt="product image" />
+        <img class="rounded-t-lg" src={src} alt="product image" />
     </a>
     <div class="px-5 pb-5">
         <a href="#">
@@ -31,7 +31,7 @@ const ProductCard = ({data}) => {
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
         </div>
         <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rs.{data.price.final_price*100}</span>
+            <span class="text-3xl font-bold text-gray-900 dark:text-white">Rs.{Math.round(data.price.final_price*50)}</span>
             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
     </div>
