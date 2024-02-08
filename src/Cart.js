12ae4d653f9b9218a7d6d40002c54512835c8a50
Shubham Bansal <shubh.6102001@gmail.com>
2024-02-08 15:45:24 +0530

    integrated orders api

--- src/Cart.js
+++ src/Cart.js
@@ -5,19 +5,26 @@ import "./cart.css";
 import { CartContext } from "./CartContext";
 import { useNavigate } from "react-router-dom";
 import Address from "./Address";
+import axios from "axios";
 
 const Cart = () => {
   const [cartEmpty, setCartEmpty] = useState(true);
-  const [showCart, setShowCart] = useState(false);
+  const [showCart, setShowCart] = useState(true);
+  const [fullAddress, setfullAddress] = useState("");
+  const [city, setcity] = useState("");
+  const [state, setstate] = useState("");
+  const [country, setcountry] = useState("");
+  const [phoneno, setphoneno] = useState(0);
+  const [pinCode, setpinCode] = useState(0);
   const navigate=useNavigate();
   const cartContext = useContext(CartContext);
   const address={
-    address:"",
-    city:"",
-    state:"",
-    country:"",
-    phoneno:0,
-    pinCode:0
+    fullAddress:fullAddress,
+    city:city,
+    state:state,
+    country:country,
+    phoneno:phoneno,
+    pinCode:pinCode
   }
 
   const order={
@@ -25,6 +32,24 @@ const Cart = () => {
     user:cartContext?.user,
     address:address,
     orderedProducts:[]
+  }
+  const proceed=async()=>{
+    order.amount=cartContext.getTotalCartAmount() > 0
+    ? (cartContext.getTotalCartAmount() * 5) / 100 +
+      cartContext.getTotalCartAmount() +
+      100
+    : 0;
+    for (const item in cartContext.cartItems) {
+      if(cartContext.cartItems[item]>0)
+      order.orderedProducts.push({productId:item,quantity:cartContext.cartItems[item]});
+    }
+    try {
+      const response=await axios.post("http://localhost:8080/order/add",order);
+      console.log(response);
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const placeOrder=()=>{
@@ -43,6 +68,7 @@ const Cart = () => {
       if(cartContext.cartItems[item]>0)
       order.orderedProducts.push({productId:item,quantity:cartContext.cartItems[item]});
     }
+    setShowCart(false)
     console.log(order);
   }
 
@@ -294,9 +320,81 @@ const Cart = () => {
       </div>}
       
 
-      <div className="w-[75%] m-auto">
-        <Address></Address>
+{ !showCart&&     <div className="w-[75%] m-auto">
+    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div class="sm:col-span-3">
+        <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+        <div class="mt-2">
+          <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
       </div>
+
+      <div class="sm:col-span-3">
+        <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+        <div class="mt-2">
+          <input type="text" name="last-name" id="last-name" autocomplete="family-name" class=" pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+
+      <div class="sm:col-span-4">
+        <label for="PhoneNo" class="block text-sm font-medium leading-6 text-gray-900">Phone No. </label>
+        <div class="mt-2">
+          <input id="PhoneNo" onChange={(e)=>setphoneno(e.target.value)} name="PhoneNo" type="number"  class=" pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+
+      <div class="sm:col-span-3">
+        <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
+        <div class="mt-2">
+          <select id="country" onChange={(e)=>setcountry(e.target.value)} name="country" autocomplete="country-name" class="block w-full outline-none rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:max-w-xs sm:text-sm sm:leading-6">
+          <option>Select Country</option>
+            <option>Bharat</option>
+            <option>United States</option>
+            <option>Canada</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-span-full">
+        <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
+        <div class="mt-2">
+          <input type="text" onChange={(e)=>setfullAddress(e.target.value)} name="street-address" id="street-address" autocomplete="street-address" class="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+
+      <div class="sm:col-span-2 sm:col-start-1">
+        <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
+        <div class="mt-2">
+          <input type="text" onChange={(e)=>setcity(e.target.value)} name="city" id="city" autocomplete="address-level2" class="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+
+      <div class="sm:col-span-2">
+        <label for="region" class="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
+        <div class="mt-2">
+          <input type="text" onChange={(e)=>setstate(e.target.value)} name="region" id="region" autocomplete="address-level1" class="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+
+      <div class="sm:col-span-2">
+        <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
+        <div class="mt-2">
+          <input type="text" onChange={(e)=>setpinCode(e.target.value)} name="postal-code" id="postal-code" autocomplete="postal-code" class="pl-4 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6">
+              </input>
+        </div>
+      </div>
+      <div class="mt-6 sm:col-span-2 flex items-center gap-x-6">
+  <button onClick={()=>setShowCart&&setShowCart(false)} class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+  <button  onClick={proceed} class="w-fit rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Submit</button>
+</div>
+    </div>
+    </div>}
 
       <Footer />
     </>
